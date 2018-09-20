146672bb4a33100d442597ca234c545d9bae023a
scuola sisini <francescomichelesisini@gmail.com>
2018-09-20 09:40:19 +0200

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,72 +1,97 @@
-<!--
-	Questo software è composto da una web application in PHP su MySQL. Lo scopo 
-	è quello di amministtrare i dati raccolti durante le indgini diagnostiche del 
-	Centro di Malattie Vascolari dell'università di Ferrara.
-	Copyright (C) <2017>  <Francesco Sisini>
+<?php
+/*
+Developer:  Ehtesham Mehmood
+Site:       PHPCodify.com
+Script:     Angularjs Login Script using PHP MySQL and Bootstrap
+File:       index.php
+*/
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
--->
-
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-.button {
-    
-    border: 1;
-    color: black;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 28px;
-    margin: 4px 2px;
-    cursor: pointer;
+$a= $_GET['a'];
+if(!isset($a)) $a="";
+if($a!="b")
+{
+    echo "<script>window.location.href = '../index.php';</script>";
 }
-</style>
-</head>
 
 
-    <body>
-        <div style="height:45px;width:100%;font-size:22;font-family:verdana;background-color:#000000;"><table style="width:100%;"><tr><td>&nbsp;</td><td style="text-align:left;"><a href='index.php' style="color:white;">Home</a></td><td style="text-align:right;">&nbsp;nbsp;<a href='imagej.html' style="color:white;">Install</a></td><td>&nbsp;</td></tr></table>
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="robots" content="noindex">
+        <title>JVP Repository Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+    </head>
+<body ng-app="AngularJSLogin" ng-controller="AngularLoginController as angCtrl">
+    
+        <div style="height:45px;width:100%;font-size:22;font-family:verdana;background-color:#000000;"><table style="width:100%;"><tr><td>&nbsp;</td><td style="text-align:left;color:white;"></td><td style="text-align:right;">&nbsp;nbsp;</td><td>&nbsp;</td></tr></table>
         </div>
-       
-       <h1 style="text-align: center; font-size:24px; font-family: Times New Roman, Georgia, Serif;font-color:blue;">Upload real time and off-line CSA dataset to produce JVP diagram</h1> 
-            
-       <div style="text-align:center;">
-           <form method="get" action="start.php">
-               <button class=button type="submit">Start</button></div>
-           </form>
-        <!--
-       <div id="gnu-banner">
-           <a href="https://www.gnu.org/">
-               <img src="img/gnu.png" size=30% alt=" [A GNU head] " />
-               <strong>GNU software</strong>
-           </a>
-       </div>
-       -->
-        
-            
-            <br><br><br><br><br><br><br><br><br><br><br><br>
-        <div  style="font-size:10px;font-family:verdana;">
-            Designed and Developed by <a href="https://github.com/francescosisini">Francesco Sisini@Tekamed srl</a><br>
-            
-        </div>
-        </div><!-- /gnu-banner -->
-        
-        
-        
+    <div class="container">
+      <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+          <div class="panel panel-info" >
+                  <div class="panel-heading">
+                      <div class="panel-title" style="text-align:center;">JVP Repository</div>
+ 
+                  </div>
+ 
+                  <div style="padding-top:30px" class="panel-body" >
+                      <form name="login" ng-submit="angCtrl.loginForm()" class="form-horizontal" method="POST">
+ 
+                          <div style="margin-bottom: 25px" class="input-group">
+                                      <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                      <input type="text" id="inputemail" class="form-control" required autofocus ng-model="angCtrl.inputData.email">
+                          </div>
+ 
+                          <div style="margin-bottom: 25px" class="input-group">
+                                      <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                      <input type="password" id="inputpassword" class="form-control" required ng-model="angCtrl.inputData.password">
+                          </div>
+                          <div class="form-group">
+                              <!-- Button -->
+                              <div class="col-sm-12 controls">
+                                  <button type="submit" class="btn btn-primary pull-left"><i class="glyphicon glyphicon-log-in"></i> Log in</button>
+                              </div>
+                          </div>
+                              <div class="alert alert-danger" ng-show="errorMsg">
+                                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                                      ×</button>
+                                  <span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;{{errorMsg}}
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+      </div>
+  </div>
+<script>
+	angular.module('AngularJSLogin', [])
+	.controller('AngularLoginController', ['$scope', '$http', function($scope, $http) {
+		this.loginForm = function() {
+ 
+			var user_data='user_email=' +this.inputData.email+'&user_password='+this.inputData.password;
+ 
+			$http({
+				method: 'POST',
+				url: 'login.php',
+				data: user_data,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			})
+			.success(function(data) {
+        console.log(data);
+				if ( data.trim() === 'correct') {
+					window.location.href = 'main.php';
+				} else {
+					$scope.errorMsg = "Invalid Email and Password";
+				}
+			})
+		}
+ 
+	}]);
+	</script>
+ <div style="text-align:center;">TEKAMED-DAA&nbsp;&copy;2018 <a href="http://tekamed.it">Tekamed</a> </div>
     </body>
 </html>
-
